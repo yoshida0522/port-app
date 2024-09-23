4bc5baa2d0de2cb9c869448d91657417ffda2f5b
yoshida0522 <kantama2006@yahoo.co.jp>
2024-09-23 12:35:22 +0900

    linehook.js修正

--- pages/api/lineWebhook.js
+++ pages/api/lineWebhook.js
@@ -1,7 +1,6 @@
 import { Client } from "@line/bot-sdk";
 import { createHmac } from "crypto";
 
-// LINEのチャネル設定
 const lineConfig = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
@@ -9,41 +8,39 @@ const lineConfig = {
 
 const client = new Client(lineConfig);
 
-// APIルートの設定
 export const config = {
   api: {
-    bodyParser: false, // LINE Webhookではbody parserを無効にする必要があります
+    bodyParser: false,
   },
 };
 
-// 匿名関数を変数に代入
 const lineWebhookHandler = async (req, res) => {
   if (req.method === "POST") {
-    const signature = req.headers["x-line-signature"];
+    try {
+      const signature = req.headers["x-line-signature"];
+      const rawBody = await getRawBody(req);
+      const validSignature = validateSignature(rawBody, signature);
 
-    // bodyの生データを取得
-    const rawBody = await getRawBody(req);
+      if (!validSignature) {
+        return res.status(403).send("Signature validation failed");
+      }
 
-    // LINE Webhookの署名を検証
-    const validSignature = validateSignature(rawBody, signature);
-    if (!validSignature) {
-      return res.status(403).send("Signature validation failed");
+      const body = JSON.parse(rawBody);
+      const events = body.events;
+
+      for (const event of events) {
+        await handleEvent(event);
+      }
+      res.status(200).end();
+    } catch (error) {
+      console.error("Error processing webhook:", error);
+      res.status(500).send("Internal Server Error");
     }
-
-    // JSONにパース
-    const body = JSON.parse(rawBody);
-    const events = body.events;
-
-    events.map((event) => {
-      handleEvent(event);
-    });
-    res.status(200).end();
   } else {
     res.status(405).send("Method Not Allowed");
   }
 };
 
-// 生のリクエストボディを取得
 const getRawBody = (req) => {
   return new Promise((resolve, reject) => {
     let data = "";
@@ -59,7 +56,6 @@ const getRawBody = (req) => {
   });
 };
 
-// 署名を検証
 const validateSignature = (body, signature) => {
   const hash = createHmac("sha256", lineConfig.channelSecret)
     .update(body)
@@ -67,15 +63,13 @@ const validateSignature = (body, signature) => {
   return hash === signature;
 };
 
-// 関数をデフォルトエクスポート
-export default lineWebhookHandler;
-
 const handleEvent = async (event) => {
   if (event.type === "message" && event.message.type === "text") {
-    // メッセージに応答する処理
     return client.replyMessage(event.replyToken, {
       type: "text",
       text: `${event.message.text}`,
     });
   }
 };
+
+export default lineWebhookHandler;
