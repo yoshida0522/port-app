7bef20fb41aba167c6a4599ad388ef4f65b5ce35
yoshida0522 <kantama2006@yahoo.co.jp>
2024-09-28 16:23:10 +0900

    uuidを削除

--- src/app/components/Signin.tsx
+++ src/app/components/Signin.tsx
@@ -4,7 +4,6 @@ import db, { auth } from "../firebase";
 import styles from "../styles/page.module.css";
 import Link from "next/link";
 import { collection, getDocs } from "firebase/firestore";
-import { v4 as uuidv4 } from "uuid";
 
 interface User {
   id: string;
@@ -18,9 +17,7 @@ function Signin() {
   const [posts, setPosts] = useState<User[]>([]);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isManegerIn, setIsManegerIn] = useState(false);
-
   const [isMounted, setIsMounted] = useState(false);
-  const stateId = uuidv4();
 
   useEffect(() => {
     setIsMounted(true);
@@ -163,11 +160,7 @@ function Signin() {
                   サインイン
                 </Button>
               </div>
-              <Link
-                href={
-                  "https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2006394583&redirect_uri=https://port-8s8b92k2f-yoshida0522s-projects.vercel.app/create&state=${stateId}&scope=openid%20profile"
-                }
-              >
+              <Link href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2006394583&redirect_uri=https://port-8s8b92k2f-yoshida0522s-projects.vercel.app/create&state=uuidv4&scope=openid%20profile">
                 LINEでログイン
               </Link>
             </form>
