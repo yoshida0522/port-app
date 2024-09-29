a1dbd599bc5ddf0ef2691617a407462b1e38163e
yoshida0522 <kantama2006@yahoo.co.jp>
2024-09-29 23:02:43 +0900

    コード修正

--- pages/api/getUserId.js
+++ pages/api/getUserId.js
@@ -26,6 +26,8 @@ export default async function handler(req, res) {
     });
 
     const tokenData = await tokenResponse.json();
+    console.log("Token Data:", tokenData); // 追加
+
     const accessToken = tokenData.access_token;
 
     // ユーザー情報を取得
