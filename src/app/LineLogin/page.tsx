b8750e5b9ee70e56cc2602df16d923eee642ba44
yoshida0522 <kantama2006@yahoo.co.jp>
2024-09-30 14:05:38 +0900

    Line認証修正

--- src/app/LineLogin/page.tsx
+++ src/app/LineLogin/page.tsx
@@ -1,31 +1,65 @@
-// "use client";
+"use client";
 
 import Link from "next/link";
-import React from "react";
+import liff from "@line/liff";
+import React, { useEffect, useState } from "react";
 import styles from "../styles/page.module.css";
 
 function Page() {
+  const [idToken, setIdToken] = useState<string | null>(null);
+  const [displayName, setDisplayName] = useState<string | null>(null);
+
+  useEffect(() => {
+    liff
+      .init({ liffId: process.env.NEXT_PUBLIC_LIFF_ID as string })
+      .then(() => {
+        console.log("LIFF init succeeded.");
+        if (liff.isLoggedIn()) {
+          const token = liff.getIDToken();
+          setIdToken(token);
+        } else {
+          liff.login();
+        }
+      })
+      .catch((e: any) => {
+        console.error("LIFF init failed.", e);
+        setIdToken("");
+      });
+
+    liff.ready.then(async () => {
+      const userProfile = await liff.getProfile();
+      console.log(userProfile);
+      setDisplayName(userProfile.displayName);
+    });
+  }, []);
+
+  if (idToken === null) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <div className={styles.lineContainer}>
-        <div className={styles.lineLoginBox}>
-          <h2 className={styles.lineLogin}>ログイン</h2>
-          {/* <form> */}
-          <Link
-            className={styles.lineLink}
-            href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2006394583&redirect_uri=https://port-app-yoshida0522s-projects.vercel.app/create&state=uuidv4&scope=profile%20openid"
-            // href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=profile%20openid"
-          >
-            <img
-              className={styles.lineButton}
-              src="/images/btn_login_base.png"
-              alt="ログインボタン"
-            />
-          </Link>
-          {/* </form> */}
-        </div>
-      </div>
+      <h1>Hello {displayName}</h1>
     </div>
+
+    // <div>
+    //   <div className={styles.lineContainer}>
+    //     <div className={styles.lineLoginBox}>
+    //       <h2 className={styles.lineLogin}>ログイン</h2>
+    //       <Link
+    //         className={styles.lineLink}
+    //         href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=2006394583&redirect_uri=https://port-app-yoshida0522s-projects.vercel.app/create&state=uuidv4&scope=profile%20openid"
+    //         // href="https://access.line.me/oauth2/v2.1/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=profile%20openid"
+    //       >
+    //         <img
+    //           className={styles.lineButton}
+    //           src="/images/btn_login_base.png"
+    //           alt="ログインボタン"
+    //         />
+    //       </Link>
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
 
