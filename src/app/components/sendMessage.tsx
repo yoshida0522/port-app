a109655d969c79d90113e338432c02b142d1c970
yoshida0522 <kantama2006@yahoo.co.jp>
2024-10-02 17:42:24 +0900

    メッセージ送信機能

---
+++ src/app/components/sendMessage.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function sendMessage() {
+  return <div>sendMessage</div>;
+}
+
+export default sendMessage;
--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -171,6 +171,78 @@ export default function Page() {
     )
       .then(() => {
         console.log("すべてのデータがFirestoreに保存されました");
+
+        //
+        //
+        // メッセージ送信
+        fetch("/send-message", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({
+            userId: user, // ユーザーのLINE ID
+            message: "こんにちは！これはテストです",
+          }),
+        });
+
+        const express = require("express");
+        const axios = require("axios");
+        const app = express();
+        app.use(express.json());
+
+        const CHANNEL_ACCESS_TOKEN =
+          "FAxhlZyOgWCI4WUZg1GrXjDwvsJCVWyJP1NJG8ywQXt9GeDvwE2KMHoPqU1mkkpxDvHpJS9e5GwdSed8ylLDbZ/rZnBLDWJd6yY2mxhwODoP/8w1EyYFM5w7GasQkXtgd9j17PWApIIK/V1ikwaGMAdB04t89/1O/w1cDnyilFU=";
+
+        app.post(
+          "/send-message",
+          (
+            req: { body: { user: any; message: any } },
+            res: {
+              send: (arg0: string) => void;
+              status: (arg0: number) => {
+                (): any;
+                new (): any;
+                send: { (arg0: string): void; new (): any };
+              };
+            }
+          ) => {
+            const { user, message } = req.body;
+
+            // Messaging APIを使ってユーザーにメッセージを送る
+            axios
+              .post(
+                "https://api.line.me/v2/bot/message/push",
+                {
+                  to: user,
+                  messages: [
+                    {
+                      type: "text",
+                      text: message,
+                    },
+                  ],
+                },
+                {
+                  headers: {
+                    Authorization: `Bearer ${CHANNEL_ACCESS_TOKEN}`,
+                    "Content-Type": "application/json",
+                  },
+                }
+              )
+              .then(() => {
+                res.send("メッセージが送信されました");
+              })
+              .catch((error: any) => {
+                console.error("メッセージ送信に失敗しました", error);
+                res.status(500).send("メッセージ送信に失敗しました");
+              });
+          }
+        );
+
+        app.listen(3000, () => {
+          console.log("Server is running on port 3000");
+        });
+        //
+        //
+        // メッセージ送信が終わったらthanksへ遷移
         router.push("/thanks");
       })
       .catch((error) => {
