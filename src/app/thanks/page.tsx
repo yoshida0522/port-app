26583217e95eda148a9de68db7efbc33bde20d9f
yoshida0522 <kantama2006@yahoo.co.jp>
2024-10-02 22:01:34 +0900

    userId取得

--- src/app/thanks/page.tsx
+++ src/app/thanks/page.tsx
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 // import styles from "./styles/page.module.css";
 
 function Page() {
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    const storedUserId = localStorage.getItem("userId");
+    setUserId(storedUserId);
+  }, []);
+
   return (
     <div>
       <h1>送信しました!</h1>
+      {userId ? <p>ユーザーID: {userId}</p> : <p>ユーザーIDが見つかりません</p>}
     </div>
   );
 }
