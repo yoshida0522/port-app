bf1302603cab3c65497b2b3b3b8935609dc11390
yoshida0522 <kantama2006@yahoo.co.jp>
2024-10-08 15:23:57 +0900

    当日以降を表示

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -109,14 +109,14 @@ const UsersPage = () => {
         // yesterday15.setDate(now.getDate() - 1);
         // yesterday15.setHours(15, 0, 0, 0);
 
-        // 当日の15時を取得
-        // const today = new Date(now);
-        // today.setHours(15, 0, 0, 0);
+        // 当日の日付を取得
+        const today = new Date(now);
+        today.setHours(0, 0, 0, 0);
 
         // 明日の日付を取得
-        const tomorrow = new Date(now);
-        tomorrow.setDate(now.getDate() + 1);
-        tomorrow.setHours(0, 0, 0, 0);
+        // const tomorrow = new Date(now);
+        // tomorrow.setDate(now.getDate() + 1);
+        // tomorrow.setHours(0, 0, 0, 0);
 
         // 翌々日の日付を取得
         // const dayAfterTomorrow = new Date(now);
@@ -137,7 +137,7 @@ const UsersPage = () => {
         // }
 
         // 明日以降の予約を表示
-        return dayDate >= tomorrow;
+        return dayDate >= today;
       });
 
       // 日付でフィルタリングされた後にユーザーIDでフィルタリング
