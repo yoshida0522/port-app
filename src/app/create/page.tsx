a46d6f948c5c3c79eab78679dae01404fb588482
yoshida0522 <kantama2006@yahoo.co.jp>
2024-10-30 21:44:05 +0900

    create修正

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,29 +1,35 @@
 "use client";
 
-import { collection, addDoc } from "firebase/firestore";
-import db from "../firebase";
+// import { collection, addDoc } from "firebase/firestore";
+// import db from "../firebase";
 import React, { useEffect, useState } from "react";
-import firebase from "firebase/compat/app";
+// import firebase from "firebase/compat/app";
 import "firebase/compat/firestore";
-import { v4 as uuidv4 } from "uuid";
+// import { v4 as uuidv4 } from "uuid";
 import { useRouter } from "next/navigation";
 import styles from "../styles/page.module.css";
-// import axios from "axios";
 import CreateForm from "../components/CreateForm/CreateForm";
 import { useAuthentication } from "../utills/useAuthentication";
 import { useHandleChange } from "../utills/useHandleChange";
 import { useSendMessage } from "../utills/useSendMessage";
 import { Day } from "../type";
+import { useHandleClick } from "../utills/useHandleClick";
 
 export default function CreatePage() {
   const router = useRouter();
   const { sendMessage, isSubmitting } = useSendMessage();
-  const [childName, setChildName] = useState("");
-  // const [isSubmitting, setIsSubmitting] = useState(false);
-  const [childClass, setChildClass] = useState("");
+  // const [childName, setChildName] = useState("");
+  // const [childClass, setChildClass] = useState("");
   const { user, idToken } = useAuthentication();
-  const { handleChange, firstDay, secondDay, thirdDay, fourthDay, fifthDay } =
-    useHandleChange();
+  const { handleChange } = useHandleChange();
+  const {
+    handleClick,
+    childName,
+    setChildName,
+    childClass,
+    setChildClass,
+    days,
+  } = useHandleClick(user, idToken);
 
   useEffect(() => {
     if (user) {
@@ -35,141 +41,115 @@ export default function CreatePage() {
     return <div>Loading...</div>;
   }
 
-  const handleClick = async (e: { preventDefault: () => void }) => {
+  const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-    // setIsSubmitting(true);
 
     if (!user || !idToken) {
       console.error("ユーザー情報が見つかりません");
       return;
     }
 
-    const days = [
-      {
-        id: uuidv4(),
-        name: childName,
-        class: childClass,
-        realStartTime: "",
-        realEndTime: "",
-        userId: user,
-        ...firstDay,
-      },
-      {
-        id: uuidv4(),
-        name: childName,
-        class: childClass,
-        realStartTime: "",
-        realEndTime: "",
-        userId: user,
-        ...secondDay,
-      },
-      {
-        id: uuidv4(),
-        name: childName,
-        class: childClass,
-        realStartTime: "",
-        realEndTime: "",
-        userId: user,
-        ...thirdDay,
-      },
-      {
-        id: uuidv4(),
-        name: childName,
-        class: childClass,
-        realStartTime: "",
-        realEndTime: "",
-        userId: user,
-        ...fourthDay,
-      },
-      {
-        id: uuidv4(),
-        name: childName,
-        class: childClass,
-        realStartTime: "",
-        realEndTime: "",
-        userId: user,
-        ...fifthDay,
-      },
-    ];
-
-    const filteredDays = days.filter(
-      (day) => day.date && day.date.trim() !== ""
-    );
-
-    if (filteredDays.length === 0) {
-      console.log("保存するデータがありません");
-      // setIsSubmitting(false);
-      return;
-    }
-
-    // バリデーションチェック: endTimeがstartTimeよりも前の場合
-    for (const day of filteredDays) {
-      if (day.endTime <= day.startTime) {
-        alert(
-          `日にち ${day.date} のお迎え時間は延長開始時間以降に設定してください`
-        );
-        // setIsSubmitting(false);
-        return;
-      }
-    }
-
-    try {
-      await Promise.all(
-        filteredDays.map((day) => {
-          return addDoc(collection(db, "posts"), {
-            days: [day],
-            firstDate: day.date,
-            timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
-          });
-        })
-      );
-
-      console.log("すべてのデータがFirestoreに保存されました");
-    } catch (error) {
-      console.error("エラーが発生しました:", error);
-    }
-
-    // try {
-    //   let message = `${days[0].name}さんの予約を\n以下の内容で受け付けました:\n\n`;
-
-    //   days.forEach((day) => {
-    //     if (day.date && day.date.trim() !== "") {
-    //       message += `日にち ${day.date}:\n`;
-    //       message += `延長開始時間 ${day.startTime}:\n`;
-    //       message += `お迎え時間 ${day.endTime}:\n`;
-    //       message += `備考 ${day.remark}:\n\n`;
-    //     }
-    //   });
-
-    //   const response = await axios.post("/api/linebot", {
-    //     userId: user,
-    //     message: message,
-    //   });
-
-    //   if (response.status === 200) {
-    //     console.log("LINEに通知が成功しました:", response.data);
-    //   } else {
-    //     console.error(
-    //       "LINEへの通知に失敗しました:",
-    //       response.status,
-    //       response.statusText
-    //     );
-    //   }
-    // } catch (error) {
-    //   console.error("LINEへの通知エラー:", error);
-    // }
-
-    // console.log("メッセージの送信が完了しました");
-    // router.push("/thanks");
+    await handleClick(e);
 
     await sendMessage(user, days as Day[]);
 
     router.push("/thanks");
   };
 
+  // const handleClick = async (e: { preventDefault: () => void }) => {
+  //   e.preventDefault();
+
+  //   if (!user || !idToken) {
+  //     console.error("ユーザー情報が見つかりません");
+  //     return;
+  //   }
+
+  // const days = [
+  //   {
+  //     id: uuidv4(),
+  //     name: childName,
+  //     class: childClass,
+  //     realStartTime: "",
+  //     realEndTime: "",
+  //     userId: user,
+  //     ...firstDay,
+  //   },
+  //   {
+  //     id: uuidv4(),
+  //     name: childName,
+  //     class: childClass,
+  //     realStartTime: "",
+  //     realEndTime: "",
+  //     userId: user,
+  //     ...secondDay,
+  //   },
+  //   {
+  //     id: uuidv4(),
+  //     name: childName,
+  //     class: childClass,
+  //     realStartTime: "",
+  //     realEndTime: "",
+  //     userId: user,
+  //     ...thirdDay,
+  //   },
+  //   {
+  //     id: uuidv4(),
+  //     name: childName,
+  //     class: childClass,
+  //     realStartTime: "",
+  //     realEndTime: "",
+  //     userId: user,
+  //     ...fourthDay,
+  //   },
+  //   {
+  //     id: uuidv4(),
+  //     name: childName,
+  //     class: childClass,
+  //     realStartTime: "",
+  //     realEndTime: "",
+  //     userId: user,
+  //     ...fifthDay,
+  //   },
+  // ];
+
+  // const filteredDays = days.filter(
+  //   (day) => day.date && day.date.trim() !== ""
+  // );
+
+  // if (filteredDays.length === 0) {
+  //   console.log("保存するデータがありません");
+  //   return;
+  // }
+
+  // バリデーションチェック: endTimeがstartTimeよりも前の場合
+  // for (const day of filteredDays) {
+  //   if (day.endTime <= day.startTime) {
+  //     alert(
+  //       `日にち ${day.date} のお迎え時間は延長開始時間以降に設定してください`
+  //     );
+  //     return;
+  //   }
+  // }
+
+  // try {
+  //   await Promise.all(
+  //     filteredDays.map((day) => {
+  //       return addDoc(collection(db, "posts"), {
+  //         days: [day],
+  //         firstDate: day.date,
+  //         timeStamp: firebase.firestore.FieldValue.serverTimestamp(),
+  //       });
+  //     })
+  //   );
+
+  //   console.log("すべてのデータがFirestoreに保存されました");
+  // } catch (error) {
+  //   console.error("エラーが発生しました:", error);
+  // }
+
   return (
     <div className={styles.createCenter}>
-      <form onSubmit={handleClick}>
+      <form onSubmit={handleSubmit}>
         <h1 className={styles.createTitle}>預かり保育申し込み</h1>
         <p className={styles.createChildName}>園児名</p>
         <input
